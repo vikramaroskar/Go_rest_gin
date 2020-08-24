88d2870a2073c696eb92e55240f6b0e5cb533c23
vikram <vikram.aroskar@gmail.com>
2020-08-24 13:02:23 +0530

    .

--- src/main.go
+++ src/main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	
+	"github.com/gin-gonic/gin"
+	
+	"example.com/user/ginpoc/src/handler"
 )
 
 func main() {
 	fmt.Println("Starting main in Gin POC")
+	
+	r := gin.Default()
+	
+	r.GET("/ping", handler.PingGet())
+	
+	
+	
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 
 }
